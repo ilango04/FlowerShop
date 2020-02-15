1927cf8a2feecd5781730192f244bc9f1d20f531
Ilango J <jeganilango04@gmail.com>
2020-02-15 17:01:51 +0530

    Add project files.

---
+++ FlowerApplication/Validator.js
@@ -0,0 +1,58 @@
+﻿function CardValidation() {
+    var regex = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+    if (!document.getElementById("txtCard").value.match(regex)) {
+        alert("Card number is invalid!");
+    }
+}
+function ValidateSecurityCode() {
+    var x = document.getElementById("txtCode").value;
+    var regex = /[0-9]{8}/;
+    if (!x.match(regex)) alert("Security code is invalid!");
+}
+function ValidateExpiryMonth() {
+    var x = document.getElementById("txtMonth").value;
+
+    if (x == null) alert("Expiry Month is invalid!");
+}
+
+function ValidateExpiryYear() {
+    var y = document.getElementById("txtYear").value;
+    if (y == null) alert("Expiry Year is invalid!");
+}
+
+function ValidateName() {
+    var x = document.getElementById("txtName").value;
+    var regex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+    if (!x.match(regex)) alert("Name is Invalid");
+}
+
+function ValidateAddress() {
+    var x = document.getElementById("txtAddress").value;
+    var regex = /^[a-zA-Z0-9\s,'-]*$/;
+    if (!x.match(regex)) alert("Address is invalid!");
+}
+
+function ValidatePhoneNo() {
+    var x = document.getElementById("txtPhone").value;
+    var regex = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+    if (!x.match(regex)) alert("Phone number is invalid!");
+}
+
+
+function ValidateFax() {
+    var x = document.getElementById("txtFax").value;
+    var regex = /^\+?[0-9]{6,}$/;
+    if (!x.match(regex)) alert("Fax number is invalid!");
+}
+
+function ValidateEmail() {
+    var x = document.getElementById("txtEmail").value;
+    var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!x.match(regex)) alert("Email is invalid!");
+}
+function ValidatePincode() {
+    var x = document.getElementById("txtPincode").value;
+    var regex = /^\d{6}$/;
+    if (!x.match(regex)) alert("Phone number is invalid!");
+}
